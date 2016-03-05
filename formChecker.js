8f139eac6512bd56a51f0ad903103d57343b1f15
James Ballard <j.lb4@live.com>
2016-03-05 14:45:24 -0600

    working on function

--- formChecker.js
+++ formChecker.js
@@ -1,16 +1,21 @@
-
+function validateEmail(email)
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 function checkform()
 {
 
-console.log('hi');
+
   var user =document.getElementsByName('username').value;
   var password = document.getElementsByName("password").value;
   var It1= document.getElementsByName("q1").value;
   var It2 = document.getElementsByName("q2").value;
   var It3 = document.getElementsByName("q3").value;
   var shipping = document.getElementsByName("sh").value;
+  alert
 
-if (!validEmail(user)) {
+if (validateEmail(user)) {
   alert("invalid email");
   return false;
 }
@@ -23,10 +28,6 @@ if (It1= ''||It2==''||It3=='') {
 
 
 }
-function validateEmail(email)
-{
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
+
 //code from:
 // http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
